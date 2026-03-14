063f276b9901f85b247447deb2bcbfb6bbe2ff06
ZiDXie <shengxie47@gmail.com>
2026-03-14 03:13:35 +0000

    Remove dart.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include "rm_manual/chassis_gimbal_shooter_cover_manual.h"
 #include "rm_manual/engineer_manual.h"
 #include "rm_manual/engineer2_manual.h"
-#include "rm_manual/dart_manual.h"
 #include "rm_manual/wheeled_balance_manual.h"
 #include "rm_manual/drone_manual.h"
 #include "rm_manual/legged_wheel_balance_manual.h"
@@ -29,8 +28,6 @@ int main(int argc, char** argv)
     manual_control = new rm_manual::EngineerManual(nh, nh_referee);
   else if (robot == "engineer2")
     manual_control = new rm_manual::Engineer2Manual(nh, nh_referee);
-  else if (robot == "dart")
-    manual_control = new rm_manual::DartManual(nh, nh_referee);
   else if (robot == "wheeled_balance")
     manual_control = new rm_manual::WheeledBalanceManual(nh, nh_referee);
   else if (robot == "legged_wheel_balance")
