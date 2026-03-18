2b16c5a52d47607f8ebe42cf5efaaae2413fd373
ZiDXie <shengxie47@gmail.com>
2026-03-18 04:29:42 +0000

    FIx.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include "rm_manual/engineer_manual.h"
 #include "rm_manual/engineer2_manual.h"
 #include "rm_manual/wheeled_balance_manual.h"
-#include "rm_manual/drone_manual.h"
 #include "rm_manual/legged_wheel_balance_manual.h"
 
 int main(int argc, char** argv)
@@ -22,8 +21,6 @@ int main(int argc, char** argv)
     manual_control = new rm_manual::ChassisGimbalShooterCoverManual(nh, nh_referee);
   else if (robot == "hero")
     manual_control = new rm_manual::ChassisGimbalShooterManual(nh, nh_referee);
-  else if (robot == "drone")
-    manual_control = new rm_manual::DroneManual(nh, nh_referee);
   else if (robot == "engineer")
     manual_control = new rm_manual::EngineerManual(nh, nh_referee);
   else if (robot == "engineer2")
